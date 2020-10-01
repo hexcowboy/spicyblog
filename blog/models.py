287d12afda0297c70add7098248def25788faa79
jackno <jacknovotny8@gmail.com>
2020-10-01 14:07:23 -0500

    implement markdown engine

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.db import models
 
+import markdown
+
 
 class Post(models.Model):
     """A blog post that is auto-created from Markdown input"""
@@ -9,8 +11,16 @@ class Post(models.Model):
                                on_delete=models.CASCADE)
     posted_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
-    body = models.TextField(null=False, blank=False)
+    body = models.TextField()
+    body_rendered = models.TextField(blank=True)
+
+    def save(self, *args, **kwargs):
+        self.body_rendered = markdown.markdown(self.body, extensions=['codehilite', 'fenced_code'])
+        super().save(*args, **kwargs)
 
     def __str__(self):
         """Use the title as the representation"""
         return self.title
+
+    class Meta:
+        ordering = ('-posted_date', )
