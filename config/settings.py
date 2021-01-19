f14dbde09272f52ae59e34addfe4992535a8c340
jackno <jacknovotny8@gmail.com>
2021-01-19 11:53:07 -0600

    make standalone blog plugin

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    'subscriptions',
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
